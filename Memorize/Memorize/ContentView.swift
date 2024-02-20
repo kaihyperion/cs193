9d426c318fc03dc1daa5ced3fc34bf0a73956feb
kaihyperion <kyperion.workmode@gmail.com>
2024-02-20 16:44:30 -0500

    commit from xcode

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             CardView(isFaceUp: true)
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
             CardView()
         }
