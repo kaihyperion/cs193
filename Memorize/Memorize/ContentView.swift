7bb91ec7b0a1d4b8bb36ce1fe649f18236c7d9ad
kaihyperion <kyperion.workmode@gmail.com>
2024-02-20 18:02:47 -0500

    Arradified the CardView() Added .onTapGesture() for tap feature along with .toggle() to change boolean values of 'isFaceUp' Created base to avoid redundancy of writing RoundedRectangle()

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["🍆","🍑","🥕","🍌","h"]
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+                
+            }
         }
 
         .foregroundColor(.orange)
@@ -23,21 +24,24 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+    @ State var isFaceUp = true
     
     var body: some View {
-        ZStack(content: {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("🍆")
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content)
                     .font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
-        })
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
